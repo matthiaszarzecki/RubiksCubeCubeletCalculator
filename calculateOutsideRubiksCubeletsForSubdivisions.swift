c38894a316d1de10b2d669fb90eb4de8dbb620ea
IcarusTyler <matthias.zarzecki@gmail.com>
2019-05-07 14:52:30 +0200

    Update calculateOutsideRubiksCubeletsForSubdivisions.swift

--- calculateOutsideRubiksCubeletsForSubdivisions.swift
+++ calculateOutsideRubiksCubeletsForSubdivisions.swift
@@ -1,12 +1,12 @@
 //Runs in a playground
 
 func calculateOutsideRubiksCubeletsForSubdivisions(_ divisions: Int) -> Int {
-    //Standard Cube has 6 sides
+    //Standard Cube has 6 sides, standard formula only works at divisions >= 1
     if divisions == 0 {
         return 6
     }
     
-    let core = 6 * Int(pow(Double(divisions - 1), 2))  //Center Cubelets are 6*1 at 2 subdivisions (basic Rubik's Cube)
+    let core = 6 * Int(pow(Double(divisions - 1), 2))  //Center Cubelets are 6*1 at 2 divisions (basic Rubik's Cube)
     let topAndBottom = 2 * (divisions * 4)             //2 times, top and bottom
     let sides = (4 * (divisions - 1))                  //Remaining sides are always 4
     return core + topAndBottom + sides
